c1312df407c40e56b4851d3cdcfa27461fd81aed
Svetlana <gubinas83@gmail.com>
2020-10-02 17:02:37 +0300

    init: start Typical Arrays Problems task, implement function min

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+exports.min = function min(array) {
+    if (arguments.length === 0 || arguments[0].length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let min = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (!isNaN(array[i]) && array[i] < min) {
+            min = array[i];
+        }
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    return min;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return 0;
+};
+
+exports.avg = function avg(array) {
+    return 0;
+};
